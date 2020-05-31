61d1f59ab3d38847d356020d5ad25447273d2b5f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-01 01:12:14 +0800

    routing app

---
+++ evea/routes/home.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template
+from evea.db import mongo
+from flask_restful import Api, Resource, fields, marshal_with, reqparse, marshal
+
+home=Blueprint('home', __name__)
+
+api = Api(home)
+
+@home.route('/', methods = ['GET'])
+def index():
+  return render_template('index.html')
+
+
+test_fields = {
+    'source':fields.String,
+    'samp':fields.String
+    }
+
+class FuzzyFoo(Resource):
+    @marshal_with(test_fields)
+    def get(self):
+        a = [{'source':'seminal','samp':'ar'},{'source':'s','samp':'e'}]
+        return a
+api.add_resource(FuzzyFoo,'/test')
