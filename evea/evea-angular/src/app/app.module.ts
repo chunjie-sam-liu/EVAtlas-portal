3d360aae0a834c315dea279aebefcd8a54dbe18f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-07 18:30:16 +0800

    add material

--- evea/evea-angular/src/app/app.module.ts
+++ evea/evea-angular/src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+// shared modules
+import { MaterialModule } from './shared/material/material.module';
+
+// routing
 import { AppRoutingModule } from './app-routing.module';
+
+// main app
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ evea/evea-angular/src/app/shared/material/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlatformModule } from '@angular/cdk/platform';
+import { ObserversModule } from '@angular/cdk/observers';
+
+// import material
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const MaterialComponents = [
+  PlatformModule,
+  ObserversModule,
+  MatSidenavModule,
+  MatButtonToggleModule,
+  MatButtonModule,
+  MatIconModule,
+  MatDividerModule,
+  MatProgressSpinnerModule,
+  MatListModule,
+  MatToolbarModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, MaterialComponents],
+  exports: [MaterialComponents],
+})
+export class MaterialModule {}
