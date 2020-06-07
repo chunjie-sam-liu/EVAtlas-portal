aa6f4eeaa7ea6b909eb7d66e6df3fd61e3582c9b
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-07 23:37:46 +0800

    api config

---
+++ evea/evea-angular/src/app/constants/api-config.ts
@@ -0,0 +1,7 @@
+export class ApiConfig {
+  public static urlBase = 'http://bioinfo.life.hust.edu.cn/EVEA';
+  public static api = ApiConfig.urlBase + '/api';
+  // home API
+  public static apiHome = ApiConfig.api + '/home';
+  public static apiHomeTest = ApiConfig.apiHome + '/test';
+}
