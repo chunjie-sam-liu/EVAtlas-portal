bc1a99ccecf47f359238bcb88b4b5a6137ec846b
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 12:02:57 +0800

    base http

--- evea/evea-angular/src/app/shared/services/base-http.service.ts
+++ evea/evea-angular/src/app/shared/services/base-http.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class BaseHttpService {
   constructor(private httpClient: HttpClient) {}
+
+  public getData(route: string): Observable<any> {
+    return this.httpClient
+      .get(this.createRoute(route, environment.apiURL))
+      .pipe(catchError(this.handleError()));
+  }
+
+  private createRoute(route: string, envURL: string): string {
+    return `${envURL}/${route}`;
+  }
+
+  private handleError(result?: any) {
+    return (err: any) => {
+      console.error(err);
+      return of(result);
+    };
+  }
 }
--- evea/evea-angular/src/environments/environment.prod.ts
+++ evea/evea-angular/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://bioinfo.life.hust.edu.cn/EVEA/api',
 };
