9fb2a634168faff96df0625697f93d8ec2e83b82
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 09:27:10 +0800

    change the title

--- evea/evea-angular/src/app/app.component.ts
+++ evea/evea-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'evea-angular';
+  title = 'Extracellular Vescile Expression Atlas';
 }
