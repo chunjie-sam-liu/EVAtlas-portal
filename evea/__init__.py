db122d632f55b9e69a4eb95dd7aec1d2171dbb6f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 15:42:55 +0800

    add production setting

--- evea/__init__.py
+++ evea/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-from evea.config import Config
+from evea.config import Config, ProductionConfig
 
 app = Flask(__name__)
 CORS(app)
 # config database
 app.config.from_object(Config)
+app.config.from_object(ProductionConfig)
 
 import evea.db
 import evea.routing
