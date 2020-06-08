154f0b66fc399c907b498407a2388979798c03d4
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 12:49:14 +0800

    send data to front end done

--- evea/evea-angular/src/app/app.component.ts
+++ evea/evea-angular/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+// testservice api
+
+import { BaseHttpService } from './shared/services/base-http.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Extracellular Vescile Expression Atlas';
+  public testData: any[];
+  public sub: Subscription;
+  constructor(private baseHttpService: BaseHttpService) {}
+
+  public getData() {
+    this.sub = this.baseHttpService.getData('home/test').subscribe((d) => {
+      this.testData = d;
+      console.log(d);
+    });
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
