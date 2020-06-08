3321691156d460671c9a626011142f7489720e46
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-09 00:42:28 +0800

    default setup

--- evea/evea-angular/src/app/layouts/default/default.module.ts
+++ evea/evea-angular/src/app/layouts/default/default.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { SamplesComponent } from 'src/app/modules/samples/samples.component';
 
 @NgModule({
   declarations: [DefaultComponent, DashboardComponent, SamplesComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SharedModule],
 })
 export class DefaultModule {}
