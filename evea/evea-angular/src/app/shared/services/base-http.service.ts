7f49d76b62b53a7f27179830b48d58bcb906a3f0
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 09:21:53 +0800

    http service

---
+++ evea/evea-angular/src/app/shared/services/base-http.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class BaseHttpService {
+  constructor(private httpClient: HttpClient) {}
+}
