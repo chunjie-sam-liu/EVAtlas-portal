cad1bb308f67b4c1010de30705952484a8262da5
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 12:30:51 +0800

    add cors

--- evea/__init__.py
+++ evea/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from evea.config import Config
 
 app = Flask(__name__)
+CORS(app)
 # config database
 app.config.from_object(Config)
 
--- runserver.py
+++ runserver.py
@@ -2,10 +2,12 @@ import os, sys
 from evea import app
 
 p = sys.argv[1]
+
+
 def runserver():
-  port = int(os.environ.get('PORT', p))
-  app.run(host='localhost', port=port)
-  app.run()
+    port = int(os.environ.get('PORT', p))
+    app.run(host='localhost', port=port)
+
 
 if __name__ == '__main__':
-  runserver()
+    runserver()
