2d07701254a8067876a92e72668a42fbe0926778
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-09 00:40:28 +0800

    app route

--- evea/evea-angular/src/app/app-routing.module.ts
+++ evea/evea-angular/src/app/app-routing.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { SamplesComponent } from './modules/samples/samples.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DefaultComponent,
     children: [
-      {
-        path: '',
-        component: DashboardComponent,
-      },
+      { path: '', component: DashboardComponent },
+      { path: 'samples', component: SamplesComponent },
     ],
   },
 ];
