74fbc98f06ab204ae4eb6052c817663cecd6b9b0
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 22:56:36 +0800

    delete test

--- evea/evea-angular/src/app/app.component.ts
+++ evea/evea-angular/src/app/app.component.ts
@@ -10,23 +10,6 @@ import { Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'Extracellular Vescile Expression Atlas';
-  public testData: any[];
-  public sub: Subscription;
-  constructor(private baseHttpService: BaseHttpService) {}
-
-  public getData() {
-    this.sub = this.baseHttpService.getData('home/test').subscribe((d) => {
-      this.testData = d;
-      console.log(d);
-    });
-  }
-
-  ngOnInit(): void {
-    this.getData();
-  }
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
 }
