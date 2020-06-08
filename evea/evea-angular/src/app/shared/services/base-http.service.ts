ab8f43de2fb4c3e495a6bdd3b444dff4e149b8af
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 12:48:56 +0800

    service done

--- evea/evea-angular/src/app/shared/services/base-http.service.ts
+++ evea/evea-angular/src/app/shared/services/base-http.service.ts
@@ -11,19 +11,18 @@ export class BaseHttpService {
 
   public getData(route: string): Observable<any> {
     return this.httpClient
-      .get(this.createRoute(route, environment.apiURL))
-      .pipe(catchError(this.handleError()));
+      .get<any>(this.createRoute(route, environment.apiURL))
+      .pipe(catchError(this.handleError<any>()));
   }
 
   private createRoute(route: string, envURL: string): string {
     return `${envURL}/${route}`;
   }
 
-  private handleError(result?: any) {
-    return (err: any) => {
-      console.log('-----------------cj---------------');
+  private handleError<T>(result?: T) {
+    return (err: any): Observable<T> => {
       console.error(err);
-      // return of(result);
+      return of(result);
     };
   }
 }
