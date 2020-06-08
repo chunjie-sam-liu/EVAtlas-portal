979b6432e5cdf77ca277b9c4472d2dc5df49a8bc
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 23:12:04 +0800

    update routing and appmodule

--- evea/evea-angular/src/app/app.module.ts
+++ evea/evea-angular/src/app/app.module.ts
@@ -2,27 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+// main app
+import { AppComponent } from './app.component';
 // share module
 import { SharedModule } from './shared/shared.module';
 
 // routing
 import { AppRoutingModule } from './app-routing.module';
 
-// main app
-import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DefaultModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule,
     SharedModule,
-    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
