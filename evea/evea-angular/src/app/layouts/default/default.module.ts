37c6413ec130e7fb99bab353d7a0f2ef0237d93c
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-08 23:20:36 +0800

    add sample pages

--- evea/evea-angular/src/app/layouts/default/default.module.ts
+++ evea/evea-angular/src/app/layouts/default/default.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
-import { RouterModule } from '@angular/router';
+import { SamplesComponent } from 'src/app/modules/samples/samples.component';
 
 @NgModule({
-  declarations: [DefaultComponent, DashboardComponent],
+  declarations: [DefaultComponent, DashboardComponent, SamplesComponent],
   imports: [CommonModule, RouterModule],
 })
 export class DefaultModule {}
