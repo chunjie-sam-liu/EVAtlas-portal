74537f3d3aa8c6ce050f091fdf7f9ef041a41a52
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-09 11:17:45 +0800

    framework done

--- evea/evea-angular/src/app/layouts/default/default.component.ts
+++ evea/evea-angular/src/app/layouts/default/default.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-default',
   templateUrl: './default.component.html',
-  styleUrls: ['./default.component.css']
+  styleUrls: ['./default.component.css'],
 })
 export class DefaultComponent implements OnInit {
+  sideBarOpen = true;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  sideBarToggle($event: any) {
+    this.sideBarOpen = !this.sideBarOpen;
   }
-
 }
