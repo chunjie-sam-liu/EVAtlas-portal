95a71a95597c9267584e47897a5e50032f0744b6
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-12 12:21:06 +0800

    sidebar

--- evea/evea-angular/src/app/app.module.ts
+++ evea/evea-angular/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
