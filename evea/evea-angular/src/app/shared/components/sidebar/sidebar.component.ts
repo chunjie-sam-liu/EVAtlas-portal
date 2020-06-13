0b7fcd53f8a74b7697fdd2776d9c1e9583fa50cf
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-13 15:17:29 +0800

    side bar

--- evea/evea-angular/src/app/shared/components/sidebar/sidebar.component.ts
+++ evea/evea-angular/src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  isExpanded = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
