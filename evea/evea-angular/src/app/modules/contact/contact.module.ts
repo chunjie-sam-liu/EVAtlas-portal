12f3b3cf3c8fd05425e8a52451b7daefa6325a51
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-13 23:43:10 +0800

    authors and contact information

--- evea/evea-angular/src/app/modules/contact/contact.module.ts
+++ evea/evea-angular/src/app/modules/contact/contact.module.ts
@@ -6,9 +6,10 @@ import { MaterialModule } from 'src/app/shared/material.module';
 import { ContactComponent } from './contact.component';
 import { ContactCardComponent } from './contact-card/contact-card.component';
 import { ContactRoutingModule } from './contact-routing.module';
+import { MaterialElevationDirective } from 'src/app/shared/directives/material-elevation.directive';
 
 @NgModule({
-  declarations: [ContactComponent, ContactCardComponent],
+  declarations: [ContactComponent, ContactCardComponent, MaterialElevationDirective],
   imports: [CommonModule, MaterialModule, ContactRoutingModule],
 })
 export class ContactModule {}
