2a01e4ab98245d7848b9ce302b6a33e539cd2ffb
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-15 16:15:43 +0800

    mongo db output

--- evea/routes/home.py
+++ evea/routes/home.py
@@ -1,24 +1,36 @@
 from flask import Blueprint, render_template
 from evea.db import mongo
-from flask_restful import Api, Resource, fields, marshal_with, reqparse, marshal
+# from flask_restful import Api, Resource, fields, marshal_with, reqparse, marshal
+from flask_restplus import fields, Api, Resource
 
-home=Blueprint('home', __name__)
+home = Blueprint('home', __name__)
 
 api = Api(home)
 
-@home.route('/', methods = ['GET'])
+
+@home.route('/', methods=['GET'])
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 
-test_fields = {
-    'source':fields.String,
-    'samp':fields.String
-    }
-
-class FuzzyFoo(Resource):
-    @marshal_with(test_fields)
+@api.route('/hello', methods=['GET'])
+class Hello(Resource):
     def get(self):
-        a = [{'source':'seminal','samp':'ar'},{'source':'s','samp':'e'}]
+        return {'hello': 'workd'}
+
+
+model = api.model('Model', {
+    'GEO_project_id': fields.String,
+    'SRA_project_id': fields.String
+})
+
+
+@api.route('/home', '/test', methods=['GET'])
+class FuzzyFoo(Resource):
+    # @api.marshal_with(model)
+    @api.marshal_list_with(model)
+    def get(self):
+        cur = mongo.db.sample_info.find().limit(5)
+        # print(list(cur))
+        a = list(cur)
         return a
-api.add_resource(FuzzyFoo,'/test')
