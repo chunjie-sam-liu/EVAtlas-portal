7152169282ce97805722d92d100e63230c3f72a0
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-18 11:48:35 +0800

    search rna

--- evea/routes/search.py
+++ evea/routes/search.py
@@ -8,13 +8,16 @@ search = Blueprint('search', __name__)
 api = Api(search)
 
 
-model = {
-    'rnaList': fields.String
+model_rnalist = {
+    'rna': fields.String
 }
 class RNAList(Resource):
-    @marshal_with(model)
-    def get(self, rna):
-        return {'rnaList': rna}
-api.add_resource(RNAList, '/rna/<string:rna>')
+    @marshal_with(model_rnalist)
+    def get(self, r):
+        m_cur = mongo.db.rna_symbol.find(
+            {'search_name': {'$regex': r}},
+            {'rna': 1, '_id': 0}).limit(5)
+        return list(m_cur)
+api.add_resource(RNAList, '/rna/<string:r>')
 
 
