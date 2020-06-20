cb6d4e4ab8dca24294500d453af94865b1160fb6
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-20 15:19:17 +0800

    dashboard

--- evea/evea-angular/src/app/modules/dashboard/stat-overview/stat-overview.component.ts
+++ evea/evea-angular/src/app/modules/dashboard/stat-overview/stat-overview.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { EChartOption } from 'echarts';
 
 @Component({
   selector: 'app-stat-overview',
   templateUrl: './stat-overview.component.html',
-  styleUrls: ['./stat-overview.component.css']
+  styleUrls: ['./stat-overview.component.css'],
 })
 export class StatOverviewComponent implements OnInit {
+  constructor() {}
+  chartOption: EChartOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+      },
+    ],
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
