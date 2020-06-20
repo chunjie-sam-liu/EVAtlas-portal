d2695ff4e1934168f019f1b18390bba467e45f3f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-20 16:32:09 +0800

    orgnaize rna

--- evea/evea-angular/src/app/modules/rna/rna.component.ts
+++ evea/evea-angular/src/app/modules/rna/rna.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import rnaTypes from 'src/app/shared/constants/rna-types';
+
 @Component({
   selector: 'app-rna',
   templateUrl: './rna.component.html',
-  styleUrls: ['./rna.component.css']
+  styleUrls: ['./rna.component.css'],
 })
 export class RnaComponent implements OnInit {
+  rnaTypes = rnaTypes;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- evea/evea-angular/src/app/modules/rna/rna.module.ts
+++ evea/evea-angular/src/app/modules/rna/rna.module.ts
@@ -5,9 +5,10 @@ import { RnaRoutingModule } from './rna-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { RnaComponent } from './rna.component';
+import { RnaTableComponent } from './rna-table/rna-table.component';
 
 @NgModule({
-  declarations: [RnaComponent],
+  declarations: [RnaComponent, RnaTableComponent],
   imports: [CommonModule, RnaRoutingModule, SharedModule],
 })
 export class RnaModule {}
