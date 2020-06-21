5567414651ba6a2faab20a27ad0336eede753ef5
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-21 16:49:19 +0800

    basic http service

--- evea/evea-angular/src/app/shared/base-http.service.ts
+++ evea/evea-angular/src/app/shared/base-http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 
@@ -9,27 +9,15 @@ import { environment } from '../../environments/environment';
 export class BaseHttpService {
   constructor(private http: HttpClient) {}
 
-  public httpOptions = {
-    headers: { 'Content-Type': 'application/json' },
-    params: {},
-  };
-
   public getData(route: string, data?: any): Observable<any> {
-    return this.http.get<any>(this.generateRoute(route, environment.apiURL), this.generateOptions(data));
-    // .pipe(catchError(this.handleError<any>()));
+    return this.http.get(this._generateRoute(route, environment.apiURL), this._generateOptions(data));
   }
 
-  private generateRoute(route: string, envURL: string): string {
+  private _generateRoute(route: string, envURL: string): string {
     return `${envURL}/${route}`;
   }
-  private generateOptions(data: any): any {
-    return (this.httpOptions.params = data);
-  }
 
-  // private handleError<T>(result?: T) {
-  //   return (err: any): Observable<T> => {
-  //     console.error(err);
-  //     return of(result);
-  //   };
-  // }
+  private _generateOptions(data?: any): any {
+    return { params: data };
+  }
 }
