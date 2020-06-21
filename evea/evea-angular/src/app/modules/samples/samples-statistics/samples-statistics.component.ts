a6f51504287f93cca2c2a4324f2d6600b85f0245
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-21 20:59:30 +0800

    mapping rate distribution

--- evea/evea-angular/src/app/modules/samples/samples-statistics/samples-statistics.component.ts
+++ evea/evea-angular/src/app/modules/samples/samples-statistics/samples-statistics.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StatApiService } from './stat-api.service';
+import { EChartOption } from 'echarts';
 
 @Component({
   selector: 'app-samples-statistics',
@@ -7,6 +8,21 @@ import { StatApiService } from './stat-api.service';
   styleUrls: ['./samples-statistics.component.css'],
 })
 export class SamplesStatisticsComponent implements OnInit {
+  mappingRate: EChartOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+      },
+    ],
+  };
   constructor(private statApiService: StatApiService) {}
 
   ngOnInit(): void {
