265097dcda13028f8df43b3c1c17fb683a8e1721
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-22 16:31:32 +0800

    automate deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2020-06-22 16:29:07
+# @DESCRIPTION:
+
+# Number of input parameters
+
+git pull
+
+source venv/bin/activate
+pip install -r requirement.txt
+
+cd evea/evea-angular/
+npm install
+
+npm run prod-build
