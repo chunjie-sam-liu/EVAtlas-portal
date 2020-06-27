fd50b5f2afc1882f116224b50c36cca225557a28
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-28 00:40:20 +0800

    stat overview

--- evea/evea-angular/src/app/modules/samples/samples-statistics/samples-statistics.component.ts
+++ evea/evea-angular/src/app/modules/samples/samples-statistics/samples-statistics.component.ts
@@ -4,6 +4,7 @@ import { EChartOption } from 'echarts';
 import { MappingDist } from 'src/app/shared/model/mapping-dist';
 import { sortBy as _sortBy, values as _values, sum as _sum } from 'lodash-es';
 import rnaType from 'src/app/shared/constants/rna-types';
+import statDistData from 'src/app/shared/constants/sample-stat-overview';
 
 @Component({
   selector: 'app-samples-statistics',
@@ -11,6 +12,9 @@ import rnaType from 'src/app/shared/constants/rna-types';
   styleUrls: ['./samples-statistics.component.css'],
 })
 export class SamplesStatisticsComponent implements OnInit {
+  statTitle = 'Tissues and samples in EVatlas';
+  statDist: EChartOption;
+
   exoMappingRateTitle = 'Exosomes mapping rate';
   exoMappingRate: EChartOption;
   exoMappingDistTitle = 'Exosomes RNA mapping distribution';
@@ -24,6 +28,8 @@ export class SamplesStatisticsComponent implements OnInit {
   constructor(private statApiService: StatApiService) {}
 
   ngOnInit(): void {
+    // stat
+    this.statDist = this._statDist(statDistData, this.statTitle);
     // get exosome data
     this.statApiService.getDist('Exosomes').subscribe((res) => {
       this.exoMappingRate = this._mappingRate(res, this.exoMappingRateTitle);
@@ -34,6 +40,69 @@ export class SamplesStatisticsComponent implements OnInit {
       this.mvMappingRate = this._mappingRate(res, this.mvMappingRateTitle);
       this.mvMappingDist = this._rnaMappingDist(res, this.mvMappingDistTitle);
     });
+  }
+  private _statDist(d: any, title: string): EChartOption {
+    return {
+      title: {
+        show: false,
+        text: title,
+      },
+      grid: {
+        top: '2%',
+        left: '10%',
+        right: '2%',
+        bottom: '20%',
+      },
+      toolbox: {
+        showTitle: true,
+        feature: {
+          data: { show: false },
+          saveAsImage: {
+            title: 'Save as image',
+          },
+        },
+      },
+      tooltip: {
+        show: true,
+        trigger: 'axis',
+      },
+      legend: {
+        data: d.legend,
+      },
+      xAxis: {
+        type: 'category',
+        show: true,
+        name: 'Tissues',
+        nameLocation: 'center',
+        nameGap: 60,
+        nameTextStyle: { fontWeight: 'bolder' },
+        axisTick: { show: false },
+        axisLabel: { show: true, interval: 0, rotate: 45 },
+        data: d.xAxis,
+      },
+      yAxis: {
+        type: 'value',
+        show: true,
+        name: 'Number of samples',
+        nameLocation: 'center',
+        nameTextStyle: { fontWeight: 'bolder' },
+        nameGap: 30,
+      },
+      series: [
+        {
+          name: d.legend[0],
+          type: 'bar',
+          stack: 'total',
+          data: d.exo,
+        },
+        {
+          name: d.legend[1],
+          type: 'bar',
+          stack: 'total',
+          data: d.mv,
+        },
+      ],
+    };
   }
 
   private _mappingRate(d: MappingDist[], title: string): EChartOption {
