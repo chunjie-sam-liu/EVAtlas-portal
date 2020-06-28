f8a1ffd8c9da8cf8740cd2f446e746df72955f62
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-28 17:08:26 +0800

    spe layout

--- evea/evea-angular/src/app/modules/specific/spe-content/spe-content.component.ts
+++ evea/evea-angular/src/app/modules/specific/spe-content/spe-content.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-spe-content',
   templateUrl: './spe-content.component.html',
-  styleUrls: ['./spe-content.component.css']
+  styleUrls: ['./spe-content.component.css'],
 })
-export class SpeContentComponent implements OnInit {
+export class SpeContentComponent implements OnInit, OnChanges {
+  @Input() tissue: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.tissue);
   }
-
 }
