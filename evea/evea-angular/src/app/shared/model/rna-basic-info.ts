64f68f6dcb9f8108a64778792b2a4d949bced291
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-28 23:14:52 +0800

    basic info

--- evea/evea-angular/src/app/shared/model/rna-basic-info.ts
+++ evea/evea-angular/src/app/shared/model/rna-basic-info.ts
@@ -1,14 +1,19 @@
 export interface RnaBasicInfo {
-  chromosome: string;
-  start: number;
-  end: number;
-  accession?: string;
-  sequence?: string;
-  premirna?: string;
-  pre_acc?: string;
-  pre_chr?: string;
-  pre_start?: number;
-  pre_end?: number;
-  pre_seq?: string;
-  family?: string;
+  GeneSymbol: string;
+  chr: string;
+  start: string;
+  end: string;
+  strand: string;
+  loci_list?: LociList[];
+  seq: string;
+  source: string;
+  pre: string;
+  query_url: string;
+}
+
+export interface LociList {
+  chr: string;
+  start: string;
+  end: string;
+  strand: string;
 }
