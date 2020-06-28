ca201add25b42945884ea9a8a66d12b42610fe20
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-06-28 19:03:15 +0800

    http get

--- evea/evea-angular/src/app/modules/specific/spe-api.service.ts
+++ evea/evea-angular/src/app/modules/specific/spe-api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BaseHttpService } from 'src/app/shared/base-http.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SpeTissue } from 'src/app/shared/model/spe-tissue';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class SpeApiService {
+export class SpeApiService extends BaseHttpService {
+  constructor(http: HttpClient) {
+    super(http);
+  }
 
-  constructor() { }
+  public getSpeTissue(type: string, exType: string, filter: string): Observable<SpeTissue> {
+    return this.getData('spe/spetype', {
+      type,
+      ex_type: exType,
+      filter,
+    }).pipe(map((res) => res.data[0]));
+  }
 }
