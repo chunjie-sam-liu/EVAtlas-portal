e20b8c3d1e0ce13950deb1f8c5a01398f635bc0c
xieguiyan <932617527@qq.com>
2020-08-02 17:30:00 +0800

    heatmap cluster

--- evea/evea-angular/src/app/modules/samples/sample-content/sample-stat/sample-stat.component.ts
+++ evea/evea-angular/src/app/modules/samples/sample-content/sample-stat/sample-stat.component.ts
@@ -4,8 +4,9 @@ import { ContentApiService } from '../content-api.service';
 import { EChartOption } from 'echarts';
 import rnaType from 'src/app/shared/constants/rna-types';
 import { MappingDist } from 'src/app/shared/model/mapping-dist';
-import { sortBy as _sortBy, values as _values, sum as _sum, indexOf } from 'lodash-es';
+import { sortBy as _sortBy, values as _values, sum as _sum, indexOf, functions } from 'lodash-es';
 import { RnaHeatmap } from 'src/app/shared/model/rna-heatmap';
+import { CDK_CONNECTED_OVERLAY_SCROLL_STRATEGY } from '@angular/cdk/overlay/overlay-directives';
 
 @Component({
   selector: 'app-sample-stat',
@@ -101,30 +102,144 @@ export class SampleStatComponent implements OnInit, OnChanges {
   private _rnaHeatmap(d: RnaHeatmap[], title: string): EChartOption {
     let yAxis=[];
     const xAxis=[];
+    const xAxis2=[];
     const data=[];
+    const data2=[];
+    let condition=[];
     console.log(d);
     d.map((v) => {
       xAxis.push(v.srr_id);
+      xAxis2.push({ "srrId": v.srr_id, "con": v.condition })
+      condition.push(v.condition);
       yAxis.push(...v.mir_lst);
     });
     yAxis=[...new Set(yAxis)];
-    // push element to plot data
-    xAxis.map((v, i) => {
-      yAxis.map((vv, ii) => {
-        data.push([i, ii, '-']);
-      });
-    });
-    console.log(data);
+    condition=[...new Set(condition)];
+
     d.map((v, i) => {
       v.mir_lst.map((vv, ii) => {
         data[i*50+yAxis.indexOf(vv)]=[i, yAxis.indexOf(vv), v.exp_lst[ii]];
       });
     });
 
+    //mean resu for each miR of single type sample
+    const dataMean=[];
+    let sum=0;
+    let dataMeanS=[];
+    let ydata=[];
+    if (condition.includes('Normal')&&(condition.length>1)) { ; } else {
+      yAxis.map((v, i) => {
+        xAxis.map((vv, ii) => {
+          if (typeof (data[ii*50+i])!="undefined") {
+            sum=data[ii*50+i][2]+sum;
+          };
+        });
+        dataMean.push({ mean: sum/(xAxis.length), mir: v });
+        sum=0;
+      })
+    };
+    dataMeanS=dataMean.sort((a, b) => { return a.mean-b.mean });
+    ydata=dataMeanS.map(function (iterm) {
+      return iterm['mir'];
+    });
+
+    //mean resu for each miR of more than 2 types sample
+    var compare=function (obj1, obj2) {
+      var val1=obj1.Con;
+      var val2=obj2.Con;
+      if (val1<val2) {
+        return -1;
+      } else if (val1>val2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+
+    let xList=[];
+    let xListN=[];
+    if (condition.includes('Normal')&&(condition.length>1)) {
+      xAxis2.map((x, i) => { xList.push({ "srrId": x.srrId, "Con": x.con, "Oi": i }) });
+      xList.sort(compare);
+      xList.map((x, i) => { xListN.push({ "srrId": x.srrId, "Con": x.Con, "Oi": x.Oi, "Ni": i }) });
+      //cancer mean & normal mean
+      let canSum=0;
+      let norSum=0;
+      let canNum=0;
+      let norNum=0;
+      let diffValue;
+      let diffList=[];
+      let diffListS=[];
+      yAxis.map((y, i) => {
+        xListN.map((x) => {
+          if (x.Con=="Cancer"||x.Con=="cancer"||x.Con=="disease") {
+            canNum=canNum+1;
+            if (typeof (data[x.Oi*50+i])!="undefined") {
+              canSum=data[x.Oi*50+i][2]+canSum;//x.Oi means xZhou, i means yZhou
+            };
+          } else {
+            norNum=norNum+1;
+            if (typeof (data[x.Oi*50+i])!="undefined") {
+              norSum=data[x.Oi*50+i][2]+norSum;//x.Oi means xZhou, i means yZhou
+            };
+          };
+        });
+        // diff value
+        diffValue=(canSum/canNum)/(norSum/norNum);
+        canSum=0;
+        norSum=0;
+        canNum=0;
+        norNum=0;
+        diffList.push({ diffV: diffValue, mir: y });
+      });
+      diffListS=diffList.sort((a, b) => { return a.diffV-b.diffV });
+      console.log(diffListS);
+      ydata=diffListS.map(function (iterm) {
+        return iterm['mir'];
+      });
+    };
+
+    //sorted data2
+    d.map((v, i) => {
+      v.mir_lst.map((vv, ii) => {
+        data2[i*50+ydata.indexOf(vv)]=[i, ydata.indexOf(vv), v.exp_lst[ii]];
+      });
+    });
+
+    //sorted data2 according xAxis
+    xListN.map((x) => {
+      data2.map((d) => {
+        if (d[0]==x.Oi&&!(d.includes('replaced'))) {
+          d[0]=x.Ni;
+          d.push('replaced');
+        };
+      });
+    });
+
+
+    data2.map((d) => {
+      if (d.includes('replaced')) {
+        d.splice(3, 1);
+      };
+    });
+    console.log(data2);
+
+    //xZhou final
+    let xAxisF=[];
+    if (xListN.length>=1) {
+      xAxisF=xListN.map(function (iterm) {
+        return iterm['srrId'];
+      });
+    } else {
+      xAxisF=xAxisF.concat(xAxis);
+    };
+
     return {
       title: {
-        show: false,
-        text: title,
+        show: true,
+        text: "Samples",
+        left: 'center',
+        bottom: '10%'
       },
       grid: {
         top: '8%',
@@ -148,17 +263,20 @@ export class SampleStatComponent implements OnInit, OnChanges {
         type: 'category',
         axisTick: { show: false },
         splitArea: { show: true },
-        data: xAxis,
+        data: xAxisF,
+        axisLabel: {
+          show: false
+        }
       },
       yAxis: {
         type: 'category',
         axisTick: { show: false },
         splitArea: { show: true },
-        data: yAxis,
+        data: ydata,
       },
       visualMap: {
         min: 0,
-        max: 10,
+        max: 100,
         calculable: true,
         orient: 'horizontal',
         left: 'center',
@@ -166,9 +284,9 @@ export class SampleStatComponent implements OnInit, OnChanges {
       },
       series: [
         {
-          name: 'Punch Card',
+          name: 'Sample ID',
           type: 'heatmap',
-          data,
+          data: data2,
           label: { show: false },
           emphasis: {
             itemStyle: {
