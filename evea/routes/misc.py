d42461f8fc28d6ef4e19f65f2c19c56887e1949a
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-08-09 17:53:39 +0800

    tcga mirna expression

--- evea/routes/misc.py
+++ evea/routes/misc.py
@@ -8,7 +8,14 @@ api = Api(misc)
 
 
 class TCGAmiRNA(Resource):
-    pass
+    def get(self, mirna):
+        condition = {"miRNA_id": mirna}
+        output = {"_id": 0}
+        mcur = mongo.db.tcga_exp_miRNA.find_one(condition, output)
+        return mcur
+
+
+api.add_resource(TCGAmiRNA, "/tcga_mirna/<string:mirna>")
 
 
 class FuncmiRNA(Resource):
