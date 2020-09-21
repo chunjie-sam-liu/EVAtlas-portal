7ad2ecb96ecbafa51e90373a1a27517c071f8b1e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-09-21 16:42:17 +0800

    update rna type dist

--- evea/evea-angular/src/app/shared/constants/rna-type-dist.ts
+++ evea/evea-angular/src/app/shared/constants/rna-type-dist.ts
@@ -1,52 +1,37 @@
 export default [
   {
     rnaType: 'rRNA',
-    exo: 35.8017,
-    mv: 15.1463,
-    total: 28.0293,
-  },
-  {
-    rnaType: 'tRNA',
-    exo: 27.9227,
-    mv: 25.9828,
-    total: 27.1927,
+    total: 38.804
   },
   {
     rnaType: 'miRNA',
-    exo: 21.8221,
-    mv: 20.4581,
-    total: 21.3088,
+    total: 25.4173
+  },
+  {
+    rnaType: 'tRNA',
+    total: 19.849
   },
   {
     rnaType: 'scRNA',
-    exo: 12.4144,
-    mv: 37.4794,
-    total: 21.8461,
+    total: 12.7177
   },
   {
     rnaType: 'piRNA',
-    exo: 0.9452,
-    mv: 0.3548,
-    total: 0.723,
+    total: 1.5302
   },
   {
     rnaType: 'snRNA',
-    exo: 0.8715,
-    mv: 0.4838,
-    total: 0.7256,
+    total: 1.0697
   },
   {
     rnaType: 'snoRNA',
-    exo: 0.2223,
-    mv: 0.0946,
-    total: 0.1742,
+    total: 0.6112
   },
   {
     rnaType: 'pRNA',
-    exo: 0.0001,
-    mv: 0.0003,
-    total: 0.0002,
-  },
-];
+    total: 0.0009
+  }
+]
+;
 
 
