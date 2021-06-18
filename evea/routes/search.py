06bee9436b27bc74b81d0c9a06ce5d414dd2508e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-06-18 17:00:04 +0800

    model rna list

--- evea/routes/search.py
+++ evea/routes/search.py
@@ -5,13 +5,22 @@ import time
 
 search = Blueprint("search", __name__)
 api = Api(search)
-model_rnalist = {"rna": fields.String}
+model_rnalist = {
+    "rna": fields.String,
+    "chr": fields.String,
+    "start": fields.Integer,
+    "end": fields.Integer,
+    "strand": fields.String,
+    # "class": fields.String,
+    "go": fields.String,
+    # "source": fields.String,
+}
 
 
 class RNAList(Resource):
     @marshal_with(model_rnalist)
     def get(self, r):
-        m_cur = mongo.db.rna_symbol.find({"search_name": {"$regex": r}}, {"rna": 1, "_id": 0}).limit(10)
+        m_cur = mongo.db.rna_symbol.find({"search_name": {"$regex": r}}).limit(10)
         return list(m_cur)
 
 
